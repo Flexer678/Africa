221b0273ecdef0e2f717925f111bf3bb6432936f
Flexer678 <81823862+Flexer678@users.noreply.github.com>
2023-01-01 16:41:08 -0600

    Update register.js

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@ const firstName = document.querySelector('.f_name')
 register.addEventListener('click',function(){
     profileName = firstName.value
     document.cookie = "first name"+ "=" + encodeURIComponent(profileName) + "; path=/";
-    window.location.href = "https://flexer678.github.io/Africa/"
+    window.location.href = "https://www.youtube.com/"
 })
 
 
